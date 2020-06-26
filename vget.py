b79d4ae3b95be4a665fdf14232695c9cd414dfb7
resueman14 <nowruz.k@yandex.ru>
2020-06-26 13:13:08 +0500

    hi

--- vget.py
+++ vget.py
@@ -1,9 +1,6 @@
 import requests, re, sys, os
 import urllib.parse
-import pyperclip, pynput
-from pynput.keyboard import Key, Controller
 
-keyboard = Controller()
 print('getting this shit --> ' + sys.argv[1])
 
 video_url = urllib.parse.quote(sys.argv[1])
@@ -12,17 +9,10 @@ try:
 	r = requests.get(url) #dsa
 	te = re.search('id="mainDlBtn" class="no-selection downloadBtn" .* href=".*&dl=1', r.text).group()
 	noamp = te.replace('amp;', '')
-	pyperclip.copy(re.search('https://.*', noamp).group()) # копирование ссылки
+	print(re.search('https://.*', noamp).group()) # копирование ссылки
 except:
 	print("ne poluchilos")
 	sys.exit()
 
-command = "dm.lnk" # требуется наличие в PATH ярлыка на Download Master 
+command = "c:\\apps\\downloadmaster\\"+ re.search(' https://.*', noamp).group() # требуется наличие в PATH ярлыка на Download Master 
 os.system(command)
-os.system(command)
-keyboard.press(Key.insert)
-keyboard.release(Key.insert)
-keyboard.press(Key.insert)
-keyboard.release(Key.insert)
-keyboard.press(Key.enter)
-keyboard.release(Key.enter)
